8abf1a5da15fe07df92ba117113f8b758a090bf4
Justin Tadlock <justin@justintadlock.com>
2013-11-08 18:37:27 -0600

    Remove discusses meta itemprop.

--- comment/comment.php
+++ comment/comment.php
@@ -1,9 +1,6 @@
 <li <?php hybrid_attr( 'comment' ); ?>>
 
 	<article>
-
-		<meta itemprop="discusses" content="<?php the_title_attribute(); ?>" />
-
 		<header class="comment-meta">
 			<?php echo get_avatar( $comment ); ?>
 			<cite <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></cite><br />
@@ -17,7 +14,6 @@
 		</div><!-- .comment-content -->
 
 		<?php hybrid_comment_reply_link(); ?>
-
 	</article>
 
 <?php /* No closing </li> is needed.  WordPress will know where to add it. */ ?>
