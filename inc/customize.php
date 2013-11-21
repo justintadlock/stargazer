386f6733856aaf19f5c8ed8cdce663fb3e6f82e8
Justin Tadlock <justin@justintadlock.com>
2013-11-21 16:13:40 -0600

    Clean up customize.php file and add inline docs.

--- inc/customize.php
+++ inc/customize.php
@@ -1,23 +1,31 @@
 <?php
+/**
+ * Handles the theme's theme customizer functionality.
+ *
+ * @package    Stargazer
+ * @author     Justin Tadlock <justin@justintadlock.com>
+ * @copyright  Copyright (c) 2013, Justin Tadlock
+ * @link       http://themehybrid.com/themes/stargazer
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
 
 /* Theme Customizer setup. */
 add_action( 'customize_register', 'stargazer_customize_register' );
 
-function stargazer_enqueue_customizer_scripts() {
-		wp_enqueue_script(
-			'stargazer-customizer',
-			trailingslashit( get_template_directory_uri() ) . 'js/customize.min.js',
-			array( 'jquery' ),
-			null,
-			true
-		);
-}
-
+/**
+ * Sets up the theme customizer sections, controls, and settings.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  object  $wp_customize
+ * @return void
+ */
 function stargazer_customize_register( $wp_customize ) {
 
 	/* Load JavaScript files. */
 	add_action( 'customize_preview_init', 'stargazer_enqueue_customizer_scripts' );
 
+	/* Enable live preview for WordPress theme features. */
 	$wp_customize->get_setting( 'blogname' )->transport              = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport       = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport      = 'postMessage';
@@ -28,12 +36,29 @@ function stargazer_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'background_repeat' )->transport     = 'postMessage';
 	$wp_customize->get_setting( 'background_attachment' )->transport = 'postMessage';
 
-
 	/* Remove the WordPress background image control. */
 	$wp_customize->remove_control( 'background_image' );
 
 	/* Add our custom background image control. */
 	$wp_customize->add_control( 
 		new Hybrid_Customize_Control_Background_Image( $wp_customize ) 
+	);
+}
+
+/**
+ * Loads theme customizer JavaScript.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function stargazer_enqueue_customizer_scripts() {
+
+	wp_enqueue_script(
+		'stargazer-customize',
+		trailingslashit( get_template_directory_uri() ) . 'js/customize.min.js',
+		array( 'jquery' ),
+		null,
+		true
 	);
 }
