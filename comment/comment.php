b183c2a2de543c261ea04b277fb6813edfc8fc6a
Justin Tadlock <justin@justintadlock.com>
2013-11-21 18:08:22 -0600

    Change textdomain to 'stargazer'.

--- comment/comment.php
+++ comment/comment.php
@@ -5,7 +5,7 @@
 			<?php echo get_avatar( $comment ); ?>
 			<cite <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></cite><br />
 			<time <?php hybrid_attr( 'comment-published' ); ?>><?php printf( __( '%s ago', 'stargazer' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?></time>
-			<a <?php hybrid_attr( 'comment-permalink' ); ?>><?php _e( 'Permalink', 'hybrid-core' ); ?></a>
+			<a <?php hybrid_attr( 'comment-permalink' ); ?>><?php _e( 'Permalink', 'stargazer' ); ?></a>
 			<?php edit_comment_link(); ?>
 		</header><!-- .comment-meta -->
 
