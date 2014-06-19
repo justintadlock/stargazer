3a3b92e5a6e17d9166c2f08208316ddaade7a1de
Justin Tadlock <justin@justintadlock.com>
2014-06-19 14:17:54 -0500

    Add sidebar title for screen readers.

--- sidebar/primary.php
+++ sidebar/primary.php
@@ -2,6 +2,13 @@
 
 	<aside <?php hybrid_attr( 'sidebar', 'primary' ); ?>>
 
+		<h3 id="sidebar-primary-title" class="screen-reader-text"><?php 
+			/* Translators: %s is the sidebar name. This is the sidebar title shown to screen readers. */
+			printf( _x( '%s Sidebar', 'sidebar title', 'stargazer' ), hybrid_get_sidebar_name( 'primary' ) ); 
+		?></h3>
+
+		<h1 class="screen-reader-text" id="sidebar-header"><?php _e( 'Sidebar', 'accessible-twin-cities' ); ?></h1>
+
 		<?php if ( is_active_sidebar( 'primary' ) ) : // If the sidebar has widgets. ?>
 
 			<?php dynamic_sidebar( 'primary' ); // Displays the primary sidebar. ?>
