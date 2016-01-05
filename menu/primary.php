b746ea2ef14d895b0d23d29cda9ea2f490157d2c
Justin Tadlock <justin@justintadlock.com>
2016-01-05 17:29:22 -0600

    Use the user-defined menu names for the menu buttons.

--- menu/primary.php
+++ menu/primary.php
@@ -3,10 +3,7 @@
 	<nav <?php hybrid_attr( 'menu', 'primary' ); ?>>
 
 		<h3 id="menu-primary-title" class="menu-toggle">
-			<button class="screen-reader-text"><?php
-				/* Translators: %s is the nav menu name. This is the nav menu title shown to screen readers. */
-				printf( _x( '%s Menu', 'nav menu title', 'stargazer' ), hybrid_get_menu_location_name( 'primary' ) ); 
-			?></button>
+			<button class="screen-reader-text"><?php echo hybrid_get_menu_name( 'primary' ); ?></button>
 		</h3><!-- .menu-toggle -->
 
 		<?php wp_nav_menu(
