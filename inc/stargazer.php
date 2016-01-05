d95eb6a5c944f367aca255bd046e203fc6eb1828
Justin Tadlock <justin@justintadlock.com>
2016-01-05 14:03:41 -0600

    Clean up `stargazer.php`.

--- inc/stargazer.php
+++ inc/stargazer.php
@@ -10,60 +10,56 @@
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-/* Register custom image sizes. */
+# Register custom image sizes.
 add_action( 'init', 'stargazer_register_image_sizes', 5 );
 
-/* Register custom menus. */
+# Register custom menus.
 add_action( 'init', 'stargazer_register_menus', 5 );
 
-// Register custom layouts.
+# Register custom layouts.
 add_action( 'hybrid_register_layouts', 'stargazer_register_layouts' );
 
-/* Register sidebars. */
+# Register sidebars.
 add_action( 'widgets_init', 'stargazer_register_sidebars', 5 );
 
-/* Add custom scripts. */
-add_action( 'wp_enqueue_scripts', 'stargazer_enqueue_scripts' );
-
-/* Register custom styles. */
-add_action( 'wp_enqueue_scripts',    'stargazer_register_styles', 0 );
+# Register custom styles.
+add_action( 'wp_enqueue_scripts',    'stargazer_register_styles',       0 );
 add_action( 'admin_enqueue_scripts', 'stargazer_admin_register_styles', 0 );
 
-// Load styles.
-add_action( 'wp_enqueue_scripts', 'stargazer_enqueue_styles' );
+# Load scripts and styles.
+add_action( 'wp_enqueue_scripts', 'stargazer_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'stargazer_enqueue_styles'  );
 
-/* Excerpt-related filters. */
+# Excerpt-related filters.
 add_filter( 'excerpt_length', 'stargazer_excerpt_length' );
 
-/* Modifies the theme layout. */
+# Modifies the theme layout.
 add_filter( 'theme_mod_theme_layout', 'stargazer_mod_theme_layout', 15 );
 
-/* Adds custom attributes to the subsidiary sidebar. */
+# Adds custom attributes to the subsidiary sidebar.
 add_filter( 'hybrid_attr_sidebar', 'stargazer_sidebar_subsidiary_class', 10, 2 );
 
-/* Appends comments link to status posts. */
+# Appends comments link to status posts.
 add_filter( 'the_content', 'stargazer_status_content', 9 ); // run before wpautop()
 
-/* Modifies the framework's infinity symbol. */
+# Modifies the framework's infinity symbol.
 add_filter( 'hybrid_aside_infinity', 'stargazer_aside_infinity' );
 
-/* Adds custom settings for the visual editor. */
+# Adds custom settings for the visual editor.
 add_filter( 'tiny_mce_before_init', 'stargazer_tiny_mce_before_init' );
 add_filter( 'mce_css',              'stargazer_mce_css'              );
 
-/* Filters the calendar output. */
+# Filters the calendar output.
 add_filter( 'get_calendar', 'stargazer_get_calendar' );
 
 # Embed wrap.
 add_filter( 'embed_oembed_html', 'stargazer_maybe_wrap_embed', 10, 2 );
 
-/* Filters the [audio] shortcode. */
+# Filters the [audio] and [video] shortcode output.
 add_filter( 'wp_audio_shortcode', 'stargazer_audio_shortcode', 10, 4 );
-
-/* Filters the [video] shortcode. */
 add_filter( 'wp_video_shortcode', 'stargazer_video_shortcode', 10, 3 );
 
-/* Filter the [video] shortcode attributes. */
+# Filter the [video] shortcode attributes.
 add_filter( 'shortcode_atts_video', 'stargazer_video_atts' );
 
 /**
@@ -75,10 +71,10 @@ add_filter( 'shortcode_atts_video', 'stargazer_video_atts' );
  */
 function stargazer_register_image_sizes() {
 
-	/* Sets the 'post-thumbnail' size. */
+	// Sets the 'post-thumbnail' size.
 	set_post_thumbnail_size( 175, 131, true );
 
-	/* Adds the 'stargazer-full' image size. */
+	// Adds the 'stargazer-full' image size.
 	add_image_size( 'stargazer-full', 1025, 500, false );
 }
 
@@ -137,6 +133,33 @@ function stargazer_register_layouts() {
 }
 
 /**
+ * Registers custom stylesheets for the front end.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function stargazer_register_styles() {
+	wp_deregister_style( 'mediaelement' );
+	wp_deregister_style( 'wp-mediaelement' );
+
+	wp_register_style( 'stargazer-fonts',        '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Open+Sans:300,400,600,700' );
+	wp_register_style( 'stargazer-mediaelement', trailingslashit( get_template_directory_uri() ) . 'css/mediaelement/mediaelement.min.css' );
+}
+
+/**
+ * Registers stylesheets for use in the admin.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function stargazer_admin_register_styles() {
+	wp_register_style( 'stargazer-fonts', '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Open+Sans:300,400,600,700' );
+	wp_register_style( 'stargazer-admin-custom-header', trailingslashit( get_template_directory_uri() ) . 'css/admin-custom-header.css' );
+}
+
+/**
  * Enqueues scripts.
  *
  * @since  1.0.0
@@ -161,21 +184,6 @@ function stargazer_enqueue_scripts() {
 }
 
 /**
- * Registers custom stylesheets for the front end.
- *
- * @since  1.0.0
- * @access public
- * @return void
- */
-function stargazer_register_styles() {
-	wp_deregister_style( 'mediaelement' );
-	wp_deregister_style( 'wp-mediaelement' );
-
-	wp_register_style( 'stargazer-fonts',        '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Open+Sans:300,400,600,700' );
-	wp_register_style( 'stargazer-mediaelement', trailingslashit( get_template_directory_uri() ) . 'css/mediaelement/mediaelement.min.css' );
-}
-
-/**
  * Loads stylesheets.
  *
  * @since  2.0.0
@@ -196,18 +204,6 @@ function stargazer_enqueue_styles() {
 }
 
 /**
- * Registers stylesheets for use in the admin.
- *
- * @since  1.0.0
- * @access public
- * @return void
- */
-function stargazer_admin_register_styles() {
-	wp_register_style( 'stargazer-fonts', '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Open+Sans:300,400,600,700' );
-	wp_register_style( 'stargazer-admin-custom-header', trailingslashit( get_template_directory_uri() ) . 'css/admin-custom-header.css' );
-}
-
-/**
  * Callback function for adding editor styles.  Use along with the add_editor_style() function.
  *
  * @since  1.0.0
@@ -216,23 +212,23 @@ function stargazer_admin_register_styles() {
  */
 function stargazer_get_editor_styles() {
 
-	/* Set up an array for the styles. */
+	// Set up an array for the styles.
 	$editor_styles = array();
 
-	/* Add the theme's editor styles. */
+	// Add the theme's editor styles.
 	$editor_styles[] = trailingslashit( get_template_directory_uri() ) . 'css/editor-style.css';
 
-	/* If a child theme, add its editor styles. Note: WP checks whether the file exists before using it. */
+	// If a child theme, add its editor styles. Note: WP checks whether the file exists before using it.
 	if ( is_child_theme() && file_exists( trailingslashit( get_stylesheet_directory() ) . 'css/editor-style.css' ) )
 		$editor_styles[] = trailingslashit( get_stylesheet_directory_uri() ) . 'css/editor-style.css';
 
-	/* Add the locale stylesheet. */
+	// Add the locale stylesheet.
 	$editor_styles[] = get_locale_stylesheet_uri();
 
-	/* Uses Ajax to display custom theme styles added via the Theme Mods API. */
+	// Uses Ajax to display custom theme styles added via the Theme Mods API.
 	$editor_styles[] = add_query_arg( 'action', 'stargazer_editor_styles', admin_url( 'admin-ajax.php' ) );
 
-	/* Return the styles. */
+	// Return the styles.
 	return $editor_styles;
 }
 
@@ -299,8 +295,8 @@ function stargazer_mod_theme_layout( $layout ) {
  */
 function stargazer_status_content( $content ) {
 
-	if ( !is_singular() && has_post_format( 'status' ) && in_the_loop() && ( have_comments() || comments_open() ) )
-		$content .= ' <a class="comments-link" href="' . get_permalink() . '">' . number_format_i18n( get_comments_number() ) . '</a>';
+	if ( ! is_singular() && has_post_format( 'status' ) && in_the_loop() && ( have_comments() || comments_open() ) )
+		$content .= sprintf( ' <a class="comments-link" href="%s">%s</a>', esc_url( get_permalink() ), number_format_i18n( get_comments_number() ) );
 
 	return $content;
 }
@@ -317,7 +313,7 @@ function stargazer_status_content( $content ) {
 function stargazer_aside_infinity( $html ) {
 
 	if ( have_comments() || comments_open() )
-		$html = ' <a class="comments-link" href="' . get_permalink() . '">' . number_format_i18n( get_comments_number() ) . '</a>';
+		$html = sprintf( ' <a class="comments-link" href="%s">%s</a>', esc_url( get_permalink() ), number_format_i18n( get_comments_number() ) );
 
 	return $html;
 }
@@ -361,10 +357,10 @@ function stargazer_sidebar_subsidiary_class( $attr, $context ) {
 			if ( 1 === $count )
 				$attr['class'] .= ' sidebar-col-1';
 
-			elseif ( !( $count % 3 ) || $count % 2 )
+			elseif ( ! ( $count % 3 ) || $count % 2 )
 				$attr['class'] .= ' sidebar-col-3';
 
-			elseif ( !( $count % 2 ) )
+			elseif ( ! ( $count % 2 ) )
 				$attr['class'] .= ' sidebar-col-2';
 		}
 	}
@@ -387,7 +383,7 @@ function stargazer_get_calendar( $calendar ) {
 /**
  * Wraps embeds with `.embed-wrap` class.
  *
- * @since  1.3.0
+ * @since  2.0.0
  * @access public
  * @param  string  $html
  * @return string
@@ -405,7 +401,7 @@ function stargazer_wrap_embed_html( $html ) {
  * @link       http://jetpack.me
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
- * @since  1.3.0
+ * @since  2.0.0
  * @access public
  * @param  string  $html
  * @param  string  $url
@@ -463,16 +459,16 @@ function stargazer_maybe_wrap_embed( $html, $url ) {
  */
 function stargazer_audio_shortcode( $html, $atts, $audio, $post_id ) {
 
-	/* Don't show in the admin. */
+	// Don't show in the admin.
 	if ( is_admin() )
 		return $html;
 
-	/* If we have an actual attachment to work with, use the ID. */
+	// If we have an actual attachment to work with, use the ID.
 	if ( is_object( $audio ) ) {
 		$attachment_id = $audio->ID;
 	}
 
-	/* Else, get the ID via the file URL. */
+	// Else, get the ID via the file URL.
 	else {
 		$extensions = join( '|', wp_get_audio_extensions() );
 
@@ -482,14 +478,14 @@ function stargazer_audio_shortcode( $html, $atts, $audio, $post_id ) {
 			$matches
 		);
 
-		if ( !empty( $matches ) )
+		if ( ! empty( $matches ) )
 			$attachment_id = hybrid_get_attachment_id_from_url( $matches[2] );
 	}
 
-	/* If an attachment ID was found. */
+	// If an attachment ID was found.
 	if ( !empty( $attachment_id ) ) {
 
-		/* Get the attachment's featured image. */
+		// Get the attachment's featured image.
 		$image = get_the_image(
 			array(
 				'post_id'      => $attachment_id,
@@ -499,17 +495,17 @@ function stargazer_audio_shortcode( $html, $atts, $audio, $post_id ) {
 			)
 		);
 
-		/* If there's no attachment featured image, see if there's one for the post. */
+		// If there's no attachment featured image, see if there's one for the post.
 		if ( empty( $image ) && !empty( $post_id ) )
 			$image = get_the_image( array( 'image_class' => 'audio-image', 'link_to_post' => false, 'echo' => false ) );
 
-		/* Add a wrapper for the audio element and image. */
-		if ( !empty( $image ) ) {
+		// Add a wrapper for the audio element and image.
+		if ( ! empty( $image ) ) {
 			$image = preg_replace( array( '/width=[\'"].+?[\'"]/i', '/height=[\'"].+?[\'"]/i' ), '', $image );
 			$html = '<div class="audio-shortcode-wrap">' . $image . $html . '</div>';
 		}
 
-		/* If not viewing an attachment page, add the media info section. */
+		// If not viewing an attachment page, add the media info section.
 		if ( !is_attachment() ) {
 			$html .= '<div class="media-shortcode-extend">';
 			$html .= '<div class="media-info audio-info">';
@@ -535,16 +531,16 @@ function stargazer_audio_shortcode( $html, $atts, $audio, $post_id ) {
  */
 function stargazer_video_shortcode( $html, $atts, $video ) {
 
-	/* Don't show on single attachment pages or in the admin. */
+	// Don't show on single attachment pages or in the admin.
 	if ( is_attachment() || is_admin() )
 		return $html;
 
-	/* If we have an actual attachment to work with, use the ID. */
+	// If we have an actual attachment to work with, use the ID.
 	if ( is_object( $video ) ) {
 		$attachment_id = $video->ID;
 	}
 
-	/* Else, get the ID via the file URL. */
+	// Else, get the ID via the file URL.
 	else {
 		$extensions = join( '|', wp_get_video_extensions() );
 
@@ -554,12 +550,12 @@ function stargazer_video_shortcode( $html, $atts, $video ) {
 			$matches
 		);
 
-		if ( !empty( $matches ) )
+		if ( ! empty( $matches ) )
 			$attachment_id = hybrid_get_attachment_id_from_url( $matches[2] );
 	}
 
-	/* If an attachment ID was found, add the media info section. */
-	if ( !empty( $attachment_id ) ) {
+	// If an attachment ID was found, add the media info section.
+	if ( ! empty( $attachment_id ) ) {
 
 		$html .= '<div class="media-shortcode-extend">';
 		$html .= '<div class="media-info video-info">';
@@ -584,37 +580,37 @@ function stargazer_video_shortcode( $html, $atts, $video ) {
  */
 function stargazer_video_atts( $out ) {
 
-	/* Don't show in the admin. */
+	// Don't show in the admin.
 	if ( is_admin() )
 		return $out;
 
-	/* Only run if the user didn't set a 'poster' image. */
+	// Only run if the user didn't set a 'poster' image.
 	if ( empty( $out['poster'] ) ) {
 
-		/* Check the 'src' attribute for an attachment file. */
-		if ( !empty( $out['src'] ) )
+		// Check the 'src' attribute for an attachment file.
+		if ( ! empty( $out['src'] ) )
 			$attachment_id = hybrid_get_attachment_id_from_url( $out['src'] );
 
-		/* If we couldn't get an attachment from the 'src' attribute, check other supported file extensions. */
+		// If we couldn't get an attachment from the 'src' attribute, check other supported file extensions.
 		if ( empty( $attachment_id ) ) {
 
 			$default_types = wp_get_video_extensions();
 
 			foreach ( $default_types as $type ) {
 
-				if ( !empty( $out[ $type ] ) ) {
+				if ( ! empty( $out[ $type ] ) ) {
 					$attachment_id = hybrid_get_attachment_id_from_url( $out[ $type ] );
 
-					if ( !empty( $attachment_id ) )
+					if ( ! empty( $attachment_id ) )
 						break;
 				}
 			}
 		}
 
-		/* If there's an attachment ID at this point. */
-		if ( !empty( $attachment_id ) ) {
+		// If there's an attachment ID at this point.
+		if ( ! empty( $attachment_id ) ) {
 
-			/* Get the attachment's featured image. */
+			// Get the attachment's featured image.
 			$image = get_the_image(
 				array(
 					'post_id'      => $attachment_id,
@@ -625,12 +621,12 @@ function stargazer_video_atts( $out ) {
 			);
 		}
 
-		/* If no image has been found and we're in the post loop, see if the current post has a featured image. */
+		// If no image has been found and we're in the post loop, see if the current post has a featured image.
 		if ( empty( $image ) && get_post() )
 			$image = get_the_image( array( 'size' => 'full', 'format' => 'array', 'echo' => false ) );
 
-		/* Set the 'poster' attribute if we have an image at this point. */
-		if ( !empty( $image ) )
+		// Set the 'poster' attribute if we have an image at this point.
+		if ( ! empty( $image ) )
 			$out['poster'] = $image['src'];
 	}
 
