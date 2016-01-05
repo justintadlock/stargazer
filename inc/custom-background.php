3a2ac9dfdc8bb6718c9352827f1d559d69701d4b
Justin Tadlock <justin@justintadlock.com>
2016-01-05 13:43:28 -0600

    Clean up `custom-background.php`.

--- inc/custom-background.php
+++ inc/custom-background.php
@@ -9,13 +9,13 @@
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-/* Call late so child themes can override. */
+# Call late so child themes can override.
 add_action( 'after_setup_theme', 'stargazer_custom_background_setup', 15 );
 
-/* Filter the background color late. */
+# Filter the background color late.
 add_filter( 'theme_mod_background_color', 'stargazer_background_color', 95 );
 
-/* Register default background images. */
+# Register default background images.
 add_filter( 'hybrid_default_backgrounds', 'stargazer_default_backgrounds', 15 );
 
 /**
@@ -38,8 +38,8 @@ function stargazer_custom_background_setup() {
 }
 
 /**
- * If the color is `ffffff` (white), return an empty string for the background color.  This is because the 
- * theme's main container's background is also white.  In this case, we drop some margins/padding so that 
+ * If the color is `ffffff` (white), return an empty string for the background color.  This is because the
+ * theme's main container's background is also white.  In this case, we drop some margins/padding so that
  * the theme design flows better and doesn't appear that we have large, empty areas.
  *
  * @since  1.0.0
@@ -76,9 +76,9 @@ function stargazer_default_backgrounds( $backgrounds ) {
 
 
 /**
- * This is a fix for when a user sets a custom background color with no custom background image.  What 
- * happens is the theme's background image hides the user-selected background color.  If a user selects a 
- * background image, we'll just use the WordPress custom background callback.  This also fixes WordPress 
+ * This is a fix for when a user sets a custom background color with no custom background image.  What
+ * happens is the theme's background image hides the user-selected background color.  If a user selects a
+ * background image, we'll just use the WordPress custom background callback.  This also fixes WordPress
  * not correctly handling the theme's default background color.
  *
  * @link http://core.trac.wordpress.org/ticket/16919
@@ -90,23 +90,23 @@ function stargazer_default_backgrounds( $backgrounds ) {
  */
 function stargazer_custom_background_callback() {
 
-	/* Get the background image. */
+	// Get the background image.
 	$image = get_background_image();
 
-	/* If there's an image, just call the normal WordPress callback. We won't do anything here. */
+	// If there's an image, just call the normal WordPress callback. We won't do anything here.
 	if ( !empty( $image ) ) {
 		_custom_background_cb();
 		return;
 	}
 
-	/* Get the background color. */
+	// Get the background color.
 	$color = get_background_color();
 
-	/* If no background color, return. */
+	// If no background color, return.
 	if ( empty( $color ) )
 		return;
 
-	/* Use 'background' instead of 'background-color'. */
+	// Use 'background' instead of 'background-color'.
 	$style = "background: #{$color};";
 
 ?>
