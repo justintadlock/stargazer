c12e181c80281d3c189e4b845dbbf969d79c7336
Justin Tadlock <justin@justintadlock.com>
2016-01-06 14:54:11 -0600

    General code cleanup.

--- footer.php
+++ footer.php
@@ -14,8 +14,8 @@
 
 				<p class="credit">
 					<?php printf(
-						/* Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link. */
-						__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'stargazer' ), 
+						// Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link. */
+						__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'stargazer' ),
 						date_i18n( 'Y' ), hybrid_get_site_link(), hybrid_get_wp_link(), hybrid_get_theme_link()
 					); ?>
 				</p><!-- .credit -->
