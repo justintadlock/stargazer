add315eaf2f3540f8bbe6afff0c77a19c2e18f78
Justin Tadlock <justin@justintadlock.com>
2016-05-17 11:06:13 -0500

    Update default primary sidebar widgets to much more useful defaults. Also, this commit introduces the `stargazer_widget_exists()` conditional tag.

--- sidebar/primary.php
+++ sidebar/primary.php
@@ -13,21 +13,50 @@
 
 		<?php else : // If the sidebar has no widgets. ?>
 
-			<?php the_widget(
-				'WP_Widget_Text',
-				array(
-					'title'  => __( 'Example Widget', 'stargazer' ),
-					/* Translators: The %s are placeholders for HTML, so the order can't be changed. */
-					'text'   => sprintf( __( 'This is an example widget to show how the Primary sidebar looks by default. You can add custom widgets from the %swidgets screen%s in the admin.', 'stargazer' ), current_user_can( 'edit_theme_options' ) ? '<a href="' . admin_url( 'widgets.php' ) . '">' : '', current_user_can( 'edit_theme_options' ) ? '</a>' : '' ),
-					'filter' => true,
-				),
-				array(
-					'before_widget' => '<section class="widget widget_text">',
-					'after_widget'  => '</section>',
-					'before_title'  => '<h3 class="widget-title">',
-					'after_title'   => '</h3>'
-				)
-			); ?>
+			<?php if ( stargazer_widget_exists( 'WP_Widget_Search' ) ) : ?>
+
+				<?php the_widget(
+					'WP_Widget_Search',
+					array(),
+					array(
+						'before_widget' => '<section class="widget widget_search">',
+						'after_widget'  => '</section>',
+						'before_title'  => '<h3 class="widget-title">',
+						'after_title'   => '</h3>'
+					)
+				); ?>
+
+			<?php endif; ?>
+
+			<?php if ( stargazer_widget_exists( 'WP_Widget_Tag_Cloud' ) ) : ?>
+
+				<?php the_widget(
+					'WP_Widget_Tag_Cloud',
+					array(),
+					array(
+						'before_widget' => '<section class="widget widget_tag_cloud">',
+						'after_widget'  => '</section>',
+						'before_title'  => '<h3 class="widget-title">',
+						'after_title'   => '</h3>'
+					)
+				); ?>
+
+			<?php endif; ?>
+
+			<?php if ( stargazer_widget_exists( 'WP_Widget_Meta' ) ) : ?>
+
+				<?php the_widget(
+					'WP_Widget_Meta',
+					array(),
+					array(
+						'before_widget' => '<section class="widget widget_meta">',
+						'after_widget'  => '</section>',
+						'before_title'  => '<h3 class="widget-title">',
+						'after_title'   => '</h3>'
+					)
+				); ?>
+
+			<?php endif; ?>
 
 		<?php endif; // End widgets check. ?>
 
