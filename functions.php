80761fb0275550049595925050f0395d8f7a5e06
Justin Tadlock <justintadlock@gmail.com>
2018-01-08 14:37:04 -0600

    Remove old `welcome.php` screen.  This was replaced with a Pro customizer section.

--- functions.php
+++ functions.php
@@ -106,7 +106,6 @@ final class Stargazer_Theme {
 		require_once( $this->dir . 'inc/custom-background.php' );
 		require_once( $this->dir . 'inc/custom-header.php'     );
 		require_once( $this->dir . 'inc/custom-colors.php'     );
-		require_once( $this->dir . 'inc/welcome.php'           );
 		require_once( $this->dir . 'inc/class-customize.php'         );
 		require_once( $this->dir . 'inc/stargazer.php'         );
 		require_once( $this->dir . 'inc/template.php'          );
